ce7a6d786576db139a74dd893f8acd9c817c9e02
jsh <jsh440274>
2020-10-27 15:56:08 +0900

    Unsolved

--- Biology_4358/src/Main.java
+++ Biology_4358/src/Main.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 
 public class Main {
+    //
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         ArrayList<String> al = new ArrayList<>();
